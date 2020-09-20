8fe7c490a26e511ac965d399398d1a8da9f6837a
Emmanuel Yasa <emyasa.emmanuel.yasa@gmail.com>
2020-09-20 23:23:58 +0800

    [Chapter 2] Added transaction management to IntegrationConfig

--- src/main/java/com/emyasa/spring/integration/java/ch2/warehouseservice/IntegrationConfig.java
+++ src/main/java/com/emyasa/spring/integration/java/ch2/warehouseservice/IntegrationConfig.java
@@ -33,6 +33,7 @@ public class IntegrationConfig {
         SimpleMessageListenerContainer messageListenerContainer = new SimpleMessageListenerContainer();
         messageListenerContainer.setConnectionFactory(connectionFactory);
         messageListenerContainer.setDestinationName(orderQueueName);
+        messageListenerContainer.setSessionTransacted(true);
         return messageListenerContainer;
     }
 
