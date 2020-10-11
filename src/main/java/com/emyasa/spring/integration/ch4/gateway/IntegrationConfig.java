488baab1214bd0781dcc8b91499132f8431dee53
Emmanuel Yasa <emyasa.emmanuel.yasa@gmail.com>
2020-10-11 21:57:48 +0800

    [Chapter 4] Remove unnecessary code

--- src/main/java/com/emyasa/spring/integration/ch4/gateway/IntegrationConfig.java
+++ src/main/java/com/emyasa/spring/integration/ch4/gateway/IntegrationConfig.java
@@ -4,25 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.integration.annotation.InboundChannelAdapter;
-import org.springframework.integration.annotation.Poller;
 import org.springframework.integration.annotation.ServiceActivator;
 import org.springframework.integration.channel.DirectChannel;
-import org.springframework.integration.channel.PublishSubscribeChannel;
-import org.springframework.integration.jms.ChannelPublishingJmsMessageListener;
-import org.springframework.integration.jms.DynamicJmsTemplate;
-import org.springframework.integration.jms.JmsInboundGateway;
 import org.springframework.integration.jms.JmsOutboundGateway;
-import org.springframework.integration.jms.JmsSendingMessageHandler;
-import org.springframework.integration.jms.config.JmsInboundChannelAdapterParser;
-import org.springframework.integration.scheduling.PollerMetadata;
-import org.springframework.jms.core.JmsTemplate;
-import org.springframework.jms.listener.SimpleMessageListenerContainer;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.MessageHandler;
-import org.springframework.scheduling.support.PeriodicTrigger;
-
-import java.util.concurrent.TimeUnit;
 
 import javax.jms.ConnectionFactory;
 
--- src/main/java/com/emyasa/spring/integration/ch4/subscribe/IntegrationConfig.java
+++ src/main/java/com/emyasa/spring/integration/ch4/subscribe/IntegrationConfig.java
@@ -5,16 +5,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.integration.annotation.ServiceActivator;
 import org.springframework.integration.channel.DirectChannel;
 import org.springframework.integration.jms.ChannelPublishingJmsMessageListener;
-import org.springframework.integration.jms.DynamicJmsTemplate;
 import org.springframework.integration.jms.JmsInboundGateway;
-import org.springframework.integration.jms.JmsSendingMessageHandler;
-import org.springframework.jms.core.JmsTemplate;
 import org.springframework.jms.listener.SimpleMessageListenerContainer;
 import org.springframework.messaging.MessageChannel;
-import org.springframework.messaging.MessageHandler;
 
 import javax.jms.ConnectionFactory;
 
@@ -56,24 +51,6 @@ public class IntegrationConfig {
 
     @Bean
     public MessageChannel inputChannel() {
-        return new DirectChannel();
-    }
-
-    // ------------------------------------------
-
-    @Bean
-    @ServiceActivator(inputChannel = "replyChannel")
-    public MessageHandler jmsOutboundAdapter() {
-        JmsTemplate jmsTemplate = new DynamicJmsTemplate();
-        jmsTemplate.setConnectionFactory(connectionFactory);
-
-        JmsSendingMessageHandler messageHandler = new JmsSendingMessageHandler(jmsTemplate);
-        messageHandler.setDestinationName(orderReplyQueue);
-        return messageHandler;
-    }
-
-    @Bean
-    public MessageChannel replyChannel() {
         return new DirectChannel();
     }
 
